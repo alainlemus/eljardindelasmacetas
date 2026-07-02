f67fb3355594b4e76519b64762914eeca7f1896c
alainlemus <alainttlm@gmail.com>
2026-07-02 17:37:38 -0600

    fix: publish livewire assets in Docker build

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,16 +15,18 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        // Confiar en todos los proxies (Dokploy/Traefik/Nginx reverse proxy)
-        Request::setTrustedProxies(
-            ['127.0.0.1', '10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16', 'REMOTE_ADDR'],
-            Request::HEADER_X_FORWARDED_FOR |
-            Request::HEADER_X_FORWARDED_HOST |
-            Request::HEADER_X_FORWARDED_PORT |
-            Request::HEADER_X_FORWARDED_PROTO |
-            Request::HEADER_X_FORWARDED_PREFIX
-        );
+        if (! app()->isLocal()) {
+            // Confiar en todos los proxies (Dokploy/Traefik/Nginx reverse proxy)
+            Request::setTrustedProxies(
+                ['127.0.0.1', '10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16', 'REMOTE_ADDR'],
+                Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO |
+                Request::HEADER_X_FORWARDED_PREFIX
+            );
 
-        URL::forceScheme('https');
+            URL::forceScheme('https');
+        }
     }
 }
